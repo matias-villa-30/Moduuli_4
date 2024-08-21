9d2e5413860d1d8357b94a2d8cd3de948a76f0b1
villa <matias.villalemes@metropolia.fi>
2024-08-21 18:02:15 +0300

    harjoitus 3 loppu

--- 3_min_max.py
+++ 3_min_max.py
@@ -1,14 +1,11 @@
-
-
 lista = []
 
 while True:
     numerot = input("Kirjoita numerot, ohjelma loppu kun se kirioita tila merkki: ")
     if numerot != "":
-
         lista.append(numerot)
-    elif numerot == "":
 
+    elif numerot == "":
         maximo = max(lista, key=int)
         minimo = min(lista, key=int)
         print(f"Maksimi numero oli: {maximo}\nMinimi numero oli: {minimo} ")
